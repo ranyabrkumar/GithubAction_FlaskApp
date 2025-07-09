591ab52069769276ea6addb230c952960aff7007
Ranya <113499706+ranyabrkumar@users.noreply.github.com>
2025-07-09 16:16:54 +0530

    production changes

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def test_client():
         response = client.get("/")
         assert response.status_code == 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
